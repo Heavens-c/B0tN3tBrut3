37a64cf535bc36ddc11f53354ca4549406cece48
Heavens <60640873+Heavens-c@users.noreply.github.com>
2023-08-10 01:26:08 +0800

    Update ScannerInstall.sh

--- ScannerInstall.sh
+++ ScannerInstall.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+
 echo -e "\e[1;32mInstalando dependencias"
-yum install cpan wget curl glibc.i686 -y
-cpan force install Parallel::ForkManager
-cpan force install IO::Socket
-cpan force install IO::Select
-sleep 2
-yum install gcc php-devel php-pear libssh2 libssh2-devel libpcap -y
+
+# Install required packages without user interaction
+yum install cpan wget curl glibc.i686 gcc php-devel php-pear libssh2 libssh2-devel libpcap -y
+
+# Install Perl modules without force install
+cpan install Parallel::ForkManager IO::Socket IO::Select
+
+# Install ssh2 PECL extension
 pecl install -f ssh2
-touch /etc/php.d/ssh2.ini
+
+# Create ssh2.ini file
 echo extension=ssh2.so > /etc/php.d/ssh2.ini
+
 echo -e "\e[1;36mTodo listo para sshbrute"
