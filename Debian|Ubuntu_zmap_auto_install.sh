6e8d6af8f25e7c9719f16a3be652a4d737be3e2e
Heavens <60640873+Heavens-c@users.noreply.github.com>
2023-08-10 01:23:07 +0800

    Update Debian|Ubuntu_zmap_auto_install.sh

--- Debian|Ubuntu_zmap_auto_install.sh
+++ Debian|Ubuntu_zmap_auto_install.sh
@@ -1,9 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-# Debian ZMap Install
+# Check if the script is being run with superuser privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root."
+  exit 1
+fi
 
-apt-get update -y
+# Update package repositories
+apt update
 
-apt-get install build-essential cmake libgmp3-dev gengetopt libpcap-dev flex byacc libjson-c-dev pkg-config libunistring-dev -y
+# Install required dependencies
+apt install -y build-essential cmake libgmp3-dev gengetopt libpcap-dev flex byacc libjson-c-dev pkg-config libunistring-dev
 
-apt-get install zmap -y
+# Install ZMap from the default repository
+apt install -y zmap
+
+# Check if ZMap installation was successful
+if [ $? -eq 0 ]; then
+  echo "ZMap has been successfully installed."
+  
+  # Make the script executable
+  chmod +x script_name.sh
+else
+  echo "ZMap installation failed."
+fi
